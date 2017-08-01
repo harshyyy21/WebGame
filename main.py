73d2d592b421a6135a5d858f108ac6e9ea203e4a
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 09:41:06 -0500

    My group and I are working on a web app for Google's CSSI program. This is the latest code for our CSSI hangman game

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import jinja2
+import os
+
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        my_template = jinja_environment.get_template("templates/home.html")
+        self.response.write(my_template.render())
+
+class HangmanHandler(webapp2.RequestHandler):
+    def get(self):
+        my_template = jinja_environment.get_template("templates/hangman.html")
+        self.response.write(my_template.render())
+
+class HighScoreHandler(webapp2.RequestHandler):
+    def get(self):
+        my_template = jinja_environment.get_template("templates/highschore.html")
+        self.response.write(my_template.render())
+
+class AboutUsHandler(webapp2.RequestHandler):
+    def get(self):
+        my_template = jinja_environment.get_template("templates/aboutus.html")
+        self.response.write(my_template.render())
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/home', MainHandler),
+    ('/hangman', HangmanHandler),
+    ('/highscore', HighScoreHandler),
+    ('aboutus', AboutUsHandler)
+], debug=True)
