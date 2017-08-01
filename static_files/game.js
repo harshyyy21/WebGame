8bc3076d6ada90b87ba318513b651f41150e8ca8
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 14:51:23 -0500

    Guesses print above line.

--- static_files/game.js
+++ static_files/game.js
@@ -1,13 +1,23 @@
+
 function myFunction(event){
   var x = event.target.value;
-  $("#demo").html(x) = x;
-  console.log(x)
-  console.log(event.target)
+  $("#correctguess").html(x);
  }
 
+function checkGuess(){
+   for()
+}
 
 $(document).ready(function(){
+  var word_list = ["cat"]
+  var word = "cat"
+  var spaces = word.length;
+  var i = 0;
 
+  while(i < spaces){
+  $("#spaces").append("__ ");
+    i++;
+  }
   $("button").click(myFunction);
 }
 )
