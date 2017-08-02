776787b8a7bc24d4c48b7c330172382d0625c333
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 10:37:43 -0500

    editing the counter and positioning in line in css(font style) js(getting it to count) and html(putting in a placeholder)

--- static_files/game.js
+++ static_files/game.js
@@ -1,8 +1,10 @@
 var word_list = ["rose", "monica", "christi", "fausto", "kevin", "oge", "gonzalo", "makenna", "amelia", "melissa",
-"patrycja", "luke", "andrew", "nicole", "marcelo", "jorge", "jigar", "harsh", "ivana", "david", "dimitri", "stevie", "spratt", "francesca", "courtney", "francisco", "andy"];
-var number = Math.round((Math.random()* word_list.length-1)+0);
+  "patrycja", "luke", "andrew", "nicole", "marcelo", "jorge", "jigar", "harsh", "ivana", "david", "dimitri", "stevie", "spratt", "francesca", "courtney", "francisco", "andy"
+];
+var number = Math.round((Math.random() * word_list.length - 1) + 0);
 var word = word_list[number];
 var spaces = word.length;
+var counter = 9
 var i = 0;
 var string1 = "";
 while (i < spaces) {
@@ -12,21 +14,23 @@ while (i < spaces) {
 
 
 function myFunction(event) {
-  var x = event.target.value;// .id gets the button id
+  var x = event.target.value; // .id gets the button id
   var res = $("#correctguess").html().concat(x);
   var rightwrong = word.indexOf(x.toLocaleLowerCase());
   $("#wrongguess").html("Wrong Guess!");
   $("#wrongguess").hide()
   if (rightwrong >= 0) {
-    for (i=0; i < word.length; i++) {
+    for (i = 0; i < word.length; i++) {
       if (x.toLocaleLowerCase() == word.charAt(i)) {
         var guess = $("#correctguess").html();
         var final = guess.substring(0, i * 3) + " " + x + " " + guess.substring((i + 1) * 3, guess.length);
         $("#correctguess").html(final);
       }
     }
-  }else{
+  } else {
     $("#wrongguess").show();
+    counter -= 1;
+    $("#lives").html(counter);
   }
 }
 
@@ -34,8 +38,9 @@ function myFunction(event) {
 
 $(document).ready(function() {
   var word_list = ["rose", "monica", "christi", "fausto", "kevin", "oge", "gonzalo", "makenna", "amelia", "melissa",
-"patrycja", "luke", "andrew", "nicole", "marcelo", "jorge", "jigar", "harsh", "ivana", "david", "dimitri", "stevie", "spratt", "francesca", "courtney", "francisco", "andy"]
-  var number = Math.round((Math.random()*word_list.length-1)+0);
+    "patrycja", "luke", "andrew", "nicole", "marcelo", "jorge", "jigar", "harsh", "ivana", "david", "dimitri", "stevie", "spratt", "francesca", "courtney", "francisco", "andy"
+  ]
+  var number = Math.round((Math.random() * word_list.length - 1) + 0);
   var word = word_list[number];
   var spaces = word.length;
   var i = 0;
