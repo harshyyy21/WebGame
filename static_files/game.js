d2dfcb42bc24a93c53a9fee427fc77cc958f08af
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 14:39:00 -0500

    debugging again

--- static_files/game.js
+++ static_files/game.js
@@ -2,58 +2,56 @@ function myFunction(word, event) {
   var x = event.target.value; // .id gets the button id
   var res = $("#correctguess").html().concat(x);
   var rightwrong = word.indexOf(x.toLocaleLowerCase());
-  var correctguess = word.length;
   $("#wrongguess").html("Wrong Guess!");
   $("#wrongguess").hide()
   if (rightwrong >= 0) {
     for (i = 0; i < word.length; i++) {
       if (x.toLocaleLowerCase() == word.charAt(i)) {
-        correctguess -= 1;
         var guess = $("#correctguess").html();
         var final = guess.substring(0, i * 3) + " " + x + " " + guess.substring((i + 1) * 3, guess.length);
         $("#correctguess").html(final);
       }
     }
-  } else {
+  }
+  else {
     $("#wrongguess").show();
     wrongcounter -= 1;
     $("#lives").html(wrongcounter);
-    if (wrongcounter = 8){
-      original.hide();
-      head.show();
-    }
-    else if(wrongcounter = 7){
-      head.hide();
-      body.show();
-    }
-    else if(wrongcounter = 6){
-      body.hide();
-      leftarm.show();
-    }
-    else if(wrongcounter = 5){
-      leftarm.hide();
-      rightarm.show();
-    }
-    else if(wrongcounter = 4){
-      rightarm.hide();
-      leftleg.show();
-    }
-    else if(wrongcounter = 3){
-      leftleg.hide();
-      rightleg.show();
-    }
-    else if(wrongcounter = 2){
-      rightleg.hide();
-      eyes.show();
-    }
-    else{
-      eyes.hide();
-      mouth.show();
-    }
-    if (correctguess == 0){
-      $("#wrongguess").html("You Win!");
-    }
   }
+}
+  //   if (wrongcounter = 8){
+  //     original.hide();
+  //     head.show();
+  //   }
+  //   else if(wrongcounter = 7){
+  //     head.hide();
+  //     body.show();
+  //   }
+  //   else if(wrongcounter = 6){
+  //     body.hide();
+  //     leftarm.show();
+  //   }
+  //   else if(wrongcounter = 5){
+  //     leftarm.hide();
+  //     rightarm.show();
+  //   }
+  //   else if(wrongcounter = 4){
+  //     rightarm.hide();
+  //     leftleg.show();
+  //   }
+  //   else if(wrongcounter = 3){
+  //     leftleg.hide();
+  //     rightleg.show();
+  //   }
+  //   else if(wrongcounter = 2){
+  //     rightleg.hide();
+  //     eyes.show();
+  //   }
+  //   else{
+  //     eyes.hide();
+  //     mouth.show();
+  //   }
+  // }
 
 
 
@@ -75,4 +73,5 @@ $(document).ready(function() {
 
   $("button").click(initialize);
 
-})
+}
+)
